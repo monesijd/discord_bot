b841f254d4ef379f040aa77d75656a6935d0a523
Brian Ku <brian950804@gmail.com>
2024-04-04 16:19:38 +0800

    完成訊息寫入檔案的功能

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
 import os
-import time
 import discord
 
-time.sleep(1)
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents = intents)
@@ -15,8 +13,12 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content == "Hello":
-        await message.channel.send("Hello, world!")
+
+    # 1. 將使用者丟進來的訊息寫入檔案
+    python_file = open('/tmp/my_python.py', 'w')
+    python_file.write(message.content)
+    python_file.close()
+
 
 # token 改放在環境變數
 client.run(os.environ['DISCORD_BOT_TOKEN'])
